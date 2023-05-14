687a9a29d2fe08fb1bffde73758fe74694b0d724
Akash Sharma <sharma.akash221@gmail.com>
2023-05-14 19:21:08 -0400

    adopt me react app full project

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+import { createRoot } from "react-dom/client";
+import SearchParams from "./SearchParams";
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+import Details from "./Details";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { useState } from "react";
+import AdoptedPetContext from "./AdoptedPetContext";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries : {
+      staleTime: Infinity,
+      cacheTime: Infinity
+    }
+  }
+})
+const App = () => {
+  const adoptedPet = useState(null);
+  return (
+      <BrowserRouter>
+        <QueryClientProvider client={queryClient}>
+          <AdoptedPetContext.Provider value={adoptedPet}>
+          <header> 
+            <Link to="/">Adopt Me!</Link>
+          </header>
+          <Routes>
+            <Route path="/details/:id" element={<Details />} />
+            <Route path="/" element={<SearchParams />} /> 
+          </Routes>
+          </AdoptedPetContext.Provider>
+        </QueryClientProvider>
+      </BrowserRouter>
+  );
+};
+
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(<App />);
